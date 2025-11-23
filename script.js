b9c77d7b7a0c82a2210888194f63884abe145605
Naruto0613 <naranbadrakh1013@gmail.com>
2025-11-23 11:59:38 +0800

    Finnished

--- script.js
+++ script.js
@@ -1,16 +1,31 @@
 import { alphabetLetters, categories, dashElement } from "./words.js";
-import { drawInitialStructure } from "./canvas.js";
+import {
+  drawBody,
+  drawHead,
+  drawInitialStructure,
+  drawLeftArm,
+  drawLeftLeg,
+  drawRightArm,
+  drawRightLeg,
+} from "./canvas.js";
+import { blastConfetti } from "./confetti.js";
 
 const categoryContainer = document.getElementById("category-container");
 const hiddenWord = document.querySelector("#hidden-word");
 const alphabetContainer = document.querySelector(".alphabet-container");
+const newGamePopup = document.querySelector(".new-game-popup");
+const NewGameButton = document.querySelector(".new-game-popup button");
 
 let secretWord = "";
+let correctGuesses = 0;
+let wrongGuesses = 0;
 
 document.addEventListener("DOMContentLoaded", () => {
   displayCategories();
   createAlphabetButtons();
   drawInitialStructure();
+
+  NewGameButton.addEventListener("click", startNewGame);
 });
 
 const displayCategories = () => {
@@ -59,6 +74,88 @@ const createAlphabetButtons = () => {
     const button = document.createElement("button");
     button.className = "letter";
     button.textContent = letter;
+    button.addEventListener("click", selectLetter);
     alphabetContainer.appendChild(button);
   });
+};
+
+const selectLetter = (e) => {
+  const letterButton = e.target;
+  const chosenLetter = letterButton.textContent;
+
+  if (secretWord.includes(chosenLetter)) {
+    revealLetters(chosenLetter);
+    if (correctGuesses === secretWord.length) {
+      displayResult(true);
+    }
+  } else {
+    wrongGuesses++;
+    drawNextPart();
+    if (wrongGuesses === 6) {
+      displayResult(false);
+    }
+  }
+
+  letterButton.disabled = true;
+};
+
+const revealLetters = (chosenLetter) => {
+  const dashes = document.querySelectorAll(".dash");
+
+  const secretWordLetters = secretWord.split("");
+
+  secretWordLetters.forEach((letter, index) => {
+    if (letter === chosenLetter) {
+      const dash = dashes[index];
+      dash.textContent = chosenLetter;
+      correctGuesses++;
+    }
+  });
+};
+
+const drawNextPart = () => {
+  const drawFunctions = [
+    drawHead,
+    drawBody,
+    drawLeftArm,
+    drawRightArm,
+    drawLeftLeg,
+    drawRightLeg,
+  ];
+
+  const drawStep = drawFunctions[wrongGuesses - 1];
+  drawStep();
+};
+
+const displayResult = (isWin) => {
+  const h2 = document.querySelector(".new-game-popup h2");
+  h2.textContent = isWin ? "You Win" : "You Lost!";
+
+  const p = document.querySelector(".new-game-popup p");
+  p.textContent = `Your chosen word was ${secretWord.toUpperCase()}`;
+
+  setTimeout(() => {
+    newGamePopup.classList.add("active");
+    if (isWin === true) {
+      blastConfetti();
+    }
+  }, 500);
+};
+
+const startNewGame = () => {
+  correctGuesses = 0;
+  wrongGuesses = 0;
+  secretWord = "";
+
+  categoryContainer.innerHTML = "";
+  alphabetContainer.innerHTML = "";
+  hiddenWord.innerHTML = "";
+
+  alphabetContainer.classList.remove("active");
+  hiddenWord.classList.remove("active");
+  newGamePopup.classList.remove("active");
+
+  displayCategories();
+  createAlphabetButtons();
+  drawInitialStructure();
 };
